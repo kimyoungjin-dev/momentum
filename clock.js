05efa3e5c812a1fb93842dad206e54400f5c8c59
kimyoungjin8649 <kyj14121@naver.com>
2020-12-04 17:37:17 +0900

    Update clock.js

--- clock.js
+++ clock.js
@@ -6,10 +6,13 @@ function gettime() {
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init() {
   gettime();
+  setInterval(gettime, 1000);
 }
 init();
