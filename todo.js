1cd399f52c027f0097434d1c48720707731d125c
kimyoungjin8649 <kyj14121@naver.com>
2020-12-04 22:46:04 +0900

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,15 @@
+const toDoForm = document.querySelector(".js-toDoForm");
+const toDoInput = form.querySelector("input");
+const toDoList = document.querySelector(".js-toDolist");
+
+f
+
+function loadtoDos(){
+  const toDos = localStorage.getItem("toDos")
+}
+
+
+unction init() {
+  loadtoDos();
+}
+init();
