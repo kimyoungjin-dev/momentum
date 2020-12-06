ad8ffae583ae2a77f56327688935ca4f9f9026d9
kimyoungjin8649 <kyj14121@naver.com>
2020-12-06 16:46:12 +0900

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,34 +1,51 @@
 const todoform = document.querySelector(".js-todoform");
 const todoinput = todoform.querySelector("input");
 const todolist = document.querySelector(".js-todolist");
+const todos = [];
 
-function painttodo(text) {
+function saveTodo() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function paintTodo(text) {
   const li = document.createElement("li");
-  const delbtn = document.createElement("button");
-  delbtn.innerText = "x";
   const span = document.createElement("span");
+  const delbtn = document.createElement("button");
+  const newid = todos.length + 1;
   span.innerText = text;
+  delbtn.innerhtml = "x";
   li.appendChild(span);
   li.appendChild(delbtn);
-  todolist.append(li);
+  todolist.appendChild(li);
+  li.id = newid;
+  const todoObj = {
+    text: text,
+    id: newid,
+  };
+  todos.push(todoObj);
+  saveTodo();
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = todoinput.value;
-  painttodo(currentValue);
+  paintTodo(currentValue);
   todoinput.value = "";
 }
 
-function loadName() {
-  const todos = localStorage.getItem("todos");
-  if (todos === null) {
+function loadTodos() {
+  const loadedTodos = localStorage.getItem("todos");
+  if (loadedTodos !== null) {
+    const parsedTodos = JSON.parse(loadedTodos);
+    parsedTodos.forEach(function (todo) {
+      paintTodo(todo.text);
+    });
   } else {
   }
 }
 
 function init() {
-  loadName();
+  loadTodos();
   todoform.addEventListener("submit", handleSubmit);
 }
 init();
